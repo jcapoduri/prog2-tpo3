d538b84acfce01c1722448cfe8f14832b938a5ab
Jorge Capoduri <jorge.capoduri@avature.net>
2016-10-26 13:02:21 -0300

    add first insert/delete

--- libs/lib.tree.avl.pas
+++ libs/lib.tree.avl.pas
@@ -90,6 +90,68 @@ implementation
   begin
   end;
 
+  procedure _balanceRight (var this : tAVLtree; pivot : idxRange);
+  begin
+  end;
+
+  function _max(a, b : integer) : integer;
+  begin
+    if a > b then
+      _max := a
+    else
+      _max := b;
+  end;
+
+  function _height (var this : tAVLtree; branchRoot : idxRange) : integer;
+  var
+    h    : integer;
+    node : tNode;
+  begin
+    if branchRoot = NULLIDX then
+      h := 0
+    else
+      begin
+        node := _get(this, branchRoot);
+        h := _max(_height(this, node.right), _height(this, node.left)) + 1;
+      end;
+    _height := h;
+  end;
+
+  procedure _detach (var this : tAVLtree; pos : idxRange; var item : tNode);
+  begin
+  end;
+
+  function _isLeaf(var node : tNode) : boolean;
+  begin
+    _isLeaf := (node.left = NULLIDX) and (node.right = NULLIDX);
+  end;
+
+  function _getBiggerFromBranch (var this : tAVLtree; var pivot : idxRange) : tKey;
+  var
+    node : tNode;
+  begin
+    node := _get(this, pivot);
+    while (node.right <> NULLIDX) do
+      begin
+        pivot := node.right;
+        node  := _get(this, pivot);
+      end;
+    _getBiggerFromBranch := node.key;
+  end;
+
+  function _getSmallerFromBranch (var this : tAVLtree; var pivot : idxRange) : tKey;
+  var
+    node : tNode;
+  begin
+    node := _get(this, pivot);
+    while (node.left <> NULLIDX) do
+      begin
+        pivot := node.left;
+        node  := _get(this, pivot);
+      end;
+    _getSmallerFromBranch := node.key;
+  end;
+
   { key helpers }
   function keyGt(key1, key2 : tKey) : boolean;
   begin
@@ -120,8 +182,8 @@ implementation
     reset(this.data);
     dataError := IOResult = 0;
 
-    assign(this.data, fullFileName + '.dat');
-    reset(this.data);
+    assign(this.control, fullFileName + '.ctrl');
+    reset(this.control);
     controlError := IOResult = 0;
     {$I+}
 
@@ -138,7 +200,21 @@ implementation
   end;
 
   procedure newEmptyTree     (var this : tAVLtree; path, filename : string);
+  var
+    fullFileName : string;
+    rc : tControlRecord;
   begin
+    fullFileName := path + filename;
+    {$I-}
+    assign(this.data, fullFileName + '.dat');
+    assign(this.data, fullFileName + '.ctrl');
+    rewrite(this.data);
+    rewrite(this.control);
+    rc.root := NULLIDX;
+    rc.erased := NULLIDX;
+    _setControl(this, rc);
+
+    _closeTree(this);
   end;
 
   function  isEmpty          (var this : tAVLtree) : boolean;
@@ -216,11 +292,85 @@ implementation
   end;
 
   procedure insert           (var this : tAVLtree; pos: idxRange; key : tKey);
+  var
+    node, parent : tNode;
+    rc           : tControlRecord;
+    auxIdx       : idxRange;
   begin
+    _openTree(this);
+    rc          := _getControl(this);
+    node.key    := key;
+    node.parent := pos;
+    auxIdx      := _append(this, node);
+
+    if pos = NULLIDX then //empty tree, insert at root
+      begin
+        rc      := _getControl(this);
+        rc.root := pos;
+      end
+    else
+      begin
+        parent := _get(this, pos);
+        if keyGt(parent.key, key) then
+          parent.right := auxIdx
+        else
+          parent.left  := auxIdx;
+        _set(this, pos, parent);
+      end;
+
+    _setControl(this, rc);
+    _closeTree(this);
   end;
 
   procedure remove           (var this: tAVLtree; pos: idxRange);
+  var
+    node, parent   : tNode;
+    rc             : tControlRecord;
+    auxIdx         : idxRange;
+    replacementKey : tKey;
   begin
+    _openTree(this);
+    node        := _get(this, pos);
+
+    if _isLeaf(node) then //easy
+      begin
+        if node.parent = NULLIDX then // is the root
+          begin
+            rc      := _getControl(this);
+            rc.root := NULLIDX;
+            _setControl(this, rc);
+          end
+        else
+          begin
+            parent := _get(this, node.parent);
+            if parent.right = pos then
+              parent.right := NULLIDX
+            else
+              parent.left  := NULLIDX;
+            _set(this, node.parent, parent);
+          end;
+        _detach(this, pos, node);
+      end
+    else
+      begin
+        if node.right = NULLIDX then
+          begin
+            auxIdx         := node.left;
+            replacementKey := _getSmallerFromBranch(this, auxIdx);
+          end
+        else
+          begin
+            auxIdx         := node.right;
+            replacementKey := _getBiggerFromBranch(this, auxIdx);
+          end;
+        remove(this, auxIdx);
+        node.key := replacementKey;
+        _set(this, pos, node);
+      end;
+
+      //TODO: balance
+
+    _closeTree(this);
   end;
 
   function  fetch            (var this : tAVLtree; pos: idxRange) : tNode;
